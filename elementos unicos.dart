b32a0d1a21cc4b0c6b2efedcdb6b23aab3eb0ea3
Isaias-alas <isaias.alas@unah.hn>
2025-02-06 00:23:13 -0600

    subir mis archivos

--- elementos unicos.dart
+++ elementos unicos.dart
@@ -1,10 +1,26 @@
 void main() {
-  List<int> listaOriginal = [1, 2, 2, 3, 4, 4, 5, 6, 6, 7];
-  List<int> listaUnica = obtenerElementosUnicos(listaOriginal);
-
-  print("Elementos únicos: $listaUnica");
+  List<int> listaOriginal = [1, 2, 2, 3, 4, 4, 5, 6, 7, 7, 8, 9, 9];
+  List<int> listaUnicos = obtenerElementosUnicos(listaOriginal);
+  print(listaUnicos);
 }
 
 List<int> obtenerElementosUnicos(List<int> lista) {
-  return lista.toSet().toList();
+  Map<int, int> contador = {};
+  List<int> unicos = [];
+
+  for (int numero in lista) {
+    if (contador.containsKey(numero)) {
+      contador[numero] = contador[numero]! + 1;
+    } else {
+      contador[numero] = 1;
+    }
+  }
+
+  contador.forEach((key, value) {
+    if (value == 1) {
+      unicos.add(key);
+    }
+  });
+
+  return unicos;
 }
